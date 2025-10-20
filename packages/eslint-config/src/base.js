878cb73f1f0c57e9ab67b33abad0893c54ecd037
ChristySchott <christyhauschild@gmail.com>
2025-10-20 08:39:54 -0300

    chore: set up eslint-config

---
+++ packages/eslint-config/src/base.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  prettierConfig,
+];
